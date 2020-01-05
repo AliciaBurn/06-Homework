3dcdb83d4b8834281ed9d8122c86896037b7f100
Alicia Burn <AliciaBurn@gmail.com>
2020-01-05 17:22:10 -0500

    Five day forcast cards

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
     var apiKey = "a3fcbb19c3da0b4a2f100792b8bc1161";
-
-        // Variable array that will store searched cities
-    // var citySearched = [];
+     // Variable array that will store searched cities
+    var citySearched = [];
 
     // init();
     // // Function that will get any saved cities from local storage
@@ -50,4 +49,43 @@ $(document).ready(function() {
             
               });
             });
-        });
+          });
+
+        function fiveDay(city) {
+          var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid="+ API_KEY;
+  
+          $.ajax ({
+              url: queryURL,
+              method: "GET",
+          }).then(function(response){
+             console.log(response);
+             $("#future-forcast").empty();
+             var counter = 1;
+ 
+             for(var i = 3; i < response.list.length; i+=8) {
+                  
+                 var card = $("<div>");
+                 card.addClass("forecastCard");
+ 
+                 var date = $("<p>");
+                 date.text(moment(moment(response.list[i].dt_txt).add(counter, 'days')).format('L'));
+                 date.appendTo(card);
+ 
+                 var symbol = $("<img>");
+                 symbol.addClass("forecastIcons");
+                 symbol.attr("src", "http://openweathermap.org/img/wn/"+response.list[i].weather[0].icon+"@2x.png");
+                 symbol.appendTo(card);
+ 
+                 var kelvin = response.list[i].main.temp_max;
+                 var fahrenheit = (kelvin - 273.15) * (9/5) + 32;
+                 var temperature = $("<p>");
+                 temperature.text("Temperature: " + fahrenheit.toFixed(1) + "°F");
+                 temperature.appendTo(card);
+ 
+                 var humidity = $("<p>");
+                 humidity.text("Humidity: " + response.list[i].main.humidity);
+                 humidity.appendTo(card);
+ 
+ 
+                 $("#fiveDayBox").append(card);
+             }
