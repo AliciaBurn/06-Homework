7f45742a5f68c24d8dfbbb02f8655c16a9adcd84
Alicia Burn <AliciaBurn@gmail.com>
2020-01-07 23:01:21 -0500

    Read Me started and 5 day forcast done

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # 06-Homework
+
+Read Me
+
+Project: Weather Dashboard
+Author: Alicia Burn
+Date: 1/6/2020
+
+******************************
+*  TABLE OF CONTENTS         *
+******************************
+1. Description
+2. Features
+3. Known Issues
+
+******************************
+*  DESCRIPTION              
+******************************
 Weather Dashboard
+    A weather dashboard application with search functionality to find current weather conditions and the future weather outlook for multiple cities. 
+
+******************************
+*  FEATURES             *
+******************************
+Weather Dashboard
+   1. User can search for weather reports by city using the openweathermap API
+   2. After searching for a city, the following information is displayed:
+        *Current temperature
+        *Current humidity
+        *Windspeed
+        *Uv index
+        *5 day forecast
+   3. Application uses icons to represent weather conditions.
+   4. Application stores previously searched for cities in localstorage and displays them to the user.
+
+
+******************************
+*  Known Issues             *
+******************************
+1. 
+2. 
--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 $(document).ready(function () {
   var apiKey = "a3fcbb19c3da0b4a2f100792b8bc1161";
-  // Variable array that will store searched cities
   var citySearched = [];
 
-  // init();
-  // // Function that will get any saved cities from local storage
-  // function init() {
-  // // Parsing the JSON string to an object
-  // var storedCities = JSON.parse(localStorage.getItem("cities"));
-  // // If cities were retrieved from localStorage, update the searchedCity array to it
-  // if (storedCities !== null) {
-  //     citySearched = storedCities;
-  // }
-  // // Call the renderCities function
-  // renderCities();
-  // }
+
 
   $("#search-btn").on("click", function (event) {
     event.preventDefault();
+
     var location = $("#city-search").val();
+    currentCityConditions(location);
 
-    localStorage.setItem("City", location);
+    getFiveDayForecast(location)
+    .then(function(forecast) {
+      appendFiveDayForecast(forecast);
+  
+  });
 
-    // localStorage.getItem("city") 
-    //     location.append("recent-search" + city);
+  function currentCityConditions(location) {
 
-
-    $(this).prev().attr("placeholder") == "City"
+    
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=" + apiKey;
 
     $.ajax({
@@ -45,25 +37,24 @@ $(document).ready(function () {
       $(".temp").text("Temp: " + response.main.temp + "F");
       $(".humidity").text("Humidity: " + response.main.humidity + "%");
       $(".wind-speed").text("Wind Speed: " + response.wind.speed + "mph");
-      
+
       var lon = response.coord.lon;
-            var lat = response.coord.lat;
-            
-            uvIndex(lat, lon);
+      var lat = response.coord.lat;
+
+      uvIndex(lat, lon);
     });
     function uvIndex(lat, lon) {
       var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-  
+
       $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+      }).then(function (response) {
         var uv = response.value;
-        $(".uv-index").html(
-          "UV Index: " + '<span class="uv-index-number">' + uv + "</span>"
+        $(".uv-index").html("UV Index: " + '<span class="uv-index-number">' + uv + "</span>"
         );
-  
-  
+
+
         if (uv < 4) {
           $(".uv-index-number").css({
             "background-color": "lime",
@@ -85,6 +76,60 @@ $(document).ready(function () {
         }
       });
     }
-  });
-});
+  }
 
+  // five day
+  function getFiveDayForecast(city) {
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&APPID=" +
+      apiKey;
+    return $.ajax({
+      url: queryURL,
+      method: "GET"
+    });
+  }
+  // append ajax call 5 day
+  function appendFiveDayForecast(forecast) {
+      for (var i = 0; i < forecast.list.length; i += 8) {
+        var date = forecast.list[i].dt_txt;
+        var formatDate = moment(date).format("L");
+        var temp = (forecast.list[i].main.temp_max - 273.15) * 1.8 + 32;
+        var humidity = forecast.list[i].main.humidity;
+        var icon = forecast.list[i].weather[0].icon;
+        var fiveDayIconURL =
+          "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        var newCard = $(
+          '<div class="card bg-light ml-0 mb-3 mr-3" style="min-width: 200px;">'
+        ).html(
+          '<div class="card-body">' +
+            '<h5 class="card-title" id="date">' +
+            formatDate +
+            "</h5>" +
+            '<img src="' +
+            fiveDayIconURL +
+            '"/>' +
+            '<div class="card-text" id="temp-humidity">' +
+            "Temperature: " +
+            temp.toFixed(2) +
+            "°F" +
+            "<br>" +
+            "Humidity: " +
+            humidity +
+            "%" +
+            "</div>" +
+            "</div>" +
+            "</div>"
+        );
+        $("#5-day-forecast").append(newCard);
+      }
+  }
+  function clear() {
+    $(".icon-image").empty();
+    $("#5-day-forecast").empty();
+    $("#city-input").val("");
+    $("#error-message").empty();
+  }
+})
+});
